09576d9a708ed58a806c08bce90d2b35afc1013f
Federico Martino <fedemartino@gmail.com>
2021-04-06 16:51:37 -0300

    primer version del ejercicio de marcos

---
+++ cash.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    float m;
+    int n, y, w, x, z;
+    
+    do
+    {
+        m = get_float ("Change owed: ");
+    }
+    while (m < 0);
+    
+    int cents = round(m * 100);
+    
+            for (y = 0; cents >= 25; y++)
+            {
+                cents = (cents - 25);
+            }
+                for (w = 0; cents >= 10; w++)
+                {
+                    cents = (cents - 10); 
+                }
+                
+                    for (x = 0; cents >= 5; x++)
+                    {
+                        cents = (cents - 5);
+                    }
+                        for (z = 0; cents >= 1; z++)
+                        {
+                            cents = (cents - 1);
+                        }
+        
+    n = (x + y + z + w);   
+    
+    printf("%i\n", n);
+   
+}
